2fcb07b812fbed6c77bdd6863a00456aa79eedd5
jcfashionforwomen <jc.fashionforwomen@gmail.com>
2025-07-05 16:58:45 +0530

    Update contentDetails.js

--- contentDetails.js
+++ contentDetails.js
@@ -143,5 +143,5 @@ let httpRequest = new XMLHttpRequest()
     }
 }
 
-httpRequest.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product/'+id, true)
-httpRequest.send()  
+httpRequest.open('GET', 'https://6856d9ca1789e182b37f3392.mockapi.io/productsapi/products/' + id, true)
+httpRequest.send()
